a00502a29204a7fb4213b10013f19a91762fb96e
Dustin Ruck <dustin.ruck@gmail.com>
2023-10-21 22:41:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const app = express();
 const db = require('./models');
 const cors = require('cors');
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use("/", routes);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
+}
   
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  // const PORT = process.env.PORT || 5000;
+  // app.listen(PORT, () => {
+  //   console.log(`Server is running on port ${PORT}`);
+  // });
 
 app.use(express.json());
 app.use(cors());

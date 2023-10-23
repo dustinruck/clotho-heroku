4ebdc5363240b267dba99688ce88a7b83a6639f0
Dustin Ruck <dustin.ruck@gmail.com>
2023-10-22 21:18:45 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,29 @@ const app = express();
 const db = require('./models');
 const cors = require('cors');
 
+// const db = require("./models");
+
+ 
+
+db.sequelize.sync().then(() => {
+
 
 
 
 app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+
+    cors({
+  
+      origin: ["https://sleepy-wave-79440-e840c0381b67.herokuapp.com","http://localhost:3000"],
+  
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE","OPTION"],
+  
+      credentials: true,
+  
+    })
+  
+  );app.use(express.urlencoded({ extended: true }));
 
 /* ADMIN ONLY */
 const categoryRouter = require("./routes/category-routes");
@@ -62,14 +79,25 @@ app.get('/', (req, res) => {
 // {alter: true} // {force: true} // <-- use as arg in .sync() below if needed
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("Server is running on port 3001");
-    });
-}) 
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("server running on port " + process.env.PORT);
+});
+})
 .catch((err) => {
     console.error("Unable to connect to database : ", err);
     process.exit(1);
   });
 
+//   const PORT = process.env.PORT || 5000;
+//   app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+//   });
 
+//   app.listen(process.env.PORT || 3000, () => {
 
+//     console.log("server running on port " + process.env.DEV_PORT);
+
+//   });
+
+// });
+})
